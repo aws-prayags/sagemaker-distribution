de18606b3646dbe4a6ab1c198c7c50a715d15980
Prayag Singh <prayags@amazon.com>
2024-03-18 11:36:11 -0700

    move ce tests to bash script

---
+++ test/test_artifacts/v1/scripts/run_sagemaker_code_editor_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+sagemaker-code-editor --version
+echo "Verified that sagemaker-code-editor is installed"
+
+# check that version output is not null - command succeeds basically
+
+# check that there are at least 3 extensions - confirm the exact ones
+
+# check that settings file is persisted
